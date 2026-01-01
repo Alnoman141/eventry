54a7cd665c4e83fb963054285c0af0266c194df3
Alnoman141 <nmnaba14@gmail.com>
2026-01-01 08:02:46 +0600

    basic html & design

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
-import Image from "next/image";
+import EventList from "@/components/landing/EventList"
+import Header from "@/components/landing/Header"
+import Image from "next/image"
 
 export default function Home() {
   return (
     <div>
-      Welcome to Eventry
+      <section className="container">
+        <Header />
+        <EventList />
+      </section>
     </div>
-  );
+  )
 }
